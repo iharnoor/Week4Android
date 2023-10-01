d26f89f9a74bc09b8e72ddb3f77a88171f04a6ef
Harnoor Singh <hsingh9@student.gsu.edu>
2023-10-01 15:02:49 -0400

    interfaces + lamda explained

--- app/src/main/java/com/google/fragmentstest/concepts/ConceptInterfaces.kt
+++ app/src/main/java/com/google/fragmentstest/concepts/ConceptInterfaces.kt
@@ -1,34 +1,42 @@
-package com.google.fragmentstest.concepts
+package com.google.appoftheday.concepts
 
-object ConceptInterfaces {
+
+interface Operation {
+    // 2 parameteres and one return type Int
+    fun perform(a: Int, b: Int): Int
+}
+
+class Multiply : Operation {
+    override fun perform(a: Int, b: Int): Int {
+        return a * b
+    }
+}
+
+// callee
+fun calculate(op: Operation, a: Int, b: Int): Int {
+    // i don't want to handle the multiply functionalte here. i am passing it in the caller
+    return op.perform(a, b)
+}
+
+val calculate1: (Int, Int) -> Int = { a, b -> a + b }
+
+object ConceptInterface {
     @JvmStatic
     fun main(args: Array<String>) {
-        val car = Car(3)
-        val aeroplane = Aeroplane(3)
+        val multiplyObject = Multiply()
 
-        println(car.move())
-        println(aeroplane.fly())
-    }
-}
+//        val returnVal = calculate(multiplyObject, 3,4)
+// caller
+        val returnVal = calculate(object : Operation {
+            override fun perform(a: Int, b: Int): Int {
+                return a / b
+            }
+        }, 3, 4)
 
-interface Vehicle {
-    fun move(): Int
-    fun fly(): Int {
-        return 0
-    }
-}
 
-class Car(private val position: Int) : Vehicle {
-    override fun move(): Int {
-        return position + 1
-    }
-}
+        print(returnVal)
 
-class Aeroplane(private val position: Int) : Vehicle {
-    override fun move(): Int {
-        return position + 1
-    }
-    override fun fly(): Int {
-        return position * 5
+//        val result = multiply(2, 3)
+//        println("Result: " + result) // Output: Result: 6
     }
 }
